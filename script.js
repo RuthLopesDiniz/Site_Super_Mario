1630f547e3a35f9870f36eeb936d00639c827715
RuthLopesDiniz <ruthma1964@hotmail.com>
2023-11-12 21:25:53 -0300

    Feat: Responsividade aplicada

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-let formulario = document.querySelector(".fale_conosco")
+let formulario = document.querySelector(".fale-conosco")
 let mascara = document.querySelector(".mascaraAll")
 
 function cliqueButton(){
-    formulario.style.left = "650px"
+    formulario.style.left = "auto"
     mascara.style.visibility = "visible"
+
+    if(formulario.style.left = "-250px"){
+        
+        formulario.style.left = "auto"
+        formulario.style.top = "25%"
+        mascara.style.visibility = "visible"
+        console.log("Aplicando responsividade")
+    }
 }
+
 
 function esconderForm(){
     formulario.style.left = "-320px"
