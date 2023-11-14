85aa89be5ada76b99c2f779a11af5aa10117c622
RuthLopesDiniz <ruthma1964@hotmail.com>
2023-11-13 22:16:45 -0300

    Feat: adicionado estilo deslizar para o formulário

--- script.js
+++ script.js
@@ -2,18 +2,13 @@ let formulario = document.querySelector(".fale-conosco")
 let mascara = document.querySelector(".mascaraAll")
 
 function cliqueButton(){
-    formulario.style.left = "auto"
-    mascara.style.visibility = "visible"
-
-    if(formulario.style.left = "-270px"){//se o estilo do formulário na esqueda for -270px, ele está em tela de celular e 
-        //vai aplicar as sequintes configurações específicas para essas telas.
+    
         
-        formulario.style.left = "auto"
-        formulario.style.transition = "left 1s linear"
+        formulario.style.left = "20%"
         mascara.style.visibility = "visible"
         console.log("Aplicando responsividade")
     }
-}
+
 
 
 function esconderForm(){
