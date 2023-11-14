98b5c915357615e3a3577aa88eb471dd8a7145ff
RuthLopesDiniz <ruthma1964@hotmail.com>
2023-11-13 21:51:16 -0300

    Feat: alterando estilo do formulário

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ function cliqueButton(){
     formulario.style.left = "auto"
     mascara.style.visibility = "visible"
 
-    if(formulario.style.left = "-250px"){//se o estilo do formulário na esqueda for -250px, ele está em tela de celular e 
+    if(formulario.style.left = "-270px"){//se o estilo do formulário na esqueda for -270px, ele está em tela de celular e 
         //vai aplicar as sequintes configurações específicas para essas telas.
         
         formulario.style.left = "auto"
-        formulario.style.top = "25%"
+        formulario.style.transition = "left 1s linear"
         mascara.style.visibility = "visible"
         console.log("Aplicando responsividade")
     }
